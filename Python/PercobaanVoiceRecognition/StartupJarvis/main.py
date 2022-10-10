17f4cfaf7f9b1b30beb8eb10c0a2a710e6143614
Aziz Naufal <azizeternaly@gmail.com>
2022-10-11 00:41:52 +0700

    Jarvis Startup V0.01

--- Python/PercobaanVoiceRecognition/StartupJarvis/main.py
+++ Python/PercobaanVoiceRecognition/StartupJarvis/main.py
@@ -1,9 +1,21 @@
 import os
 from Python.PercobaanVoiceRecognition.PersonalAssistant.head import Head 
-import winsound
 
-jarvisHead = Head("en", 180)
+import time
+from jazzit import waiting_track
 
-text = "Wellcome Sir! All System are ready to lunch!"
-jarvisHead.speak(text)
+@waiting_track("D:\MINE\Project\Personal\Mainan\Python\PercobaanVoiceRecognition\\asset\sound\mixkit-sci-fi-drone-hover-915.wav")
 
+def run():
+    jarvisHead = Head("en", 180)
+    time.sleep(5)
+    text = "Wellcome Sir!"
+    jarvisHead.speak(text)
+    time.sleep(0.5)
+    text = " All System are ready to lunch!"
+    jarvisHead.speak(text)
+    time.sleep(2)
+
+
+if __name__ == "__main__":
+    run()
