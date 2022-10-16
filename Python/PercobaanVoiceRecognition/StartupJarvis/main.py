2ec6dd6ba34c8e2c8eb269196b691ea3c5470a19
Aziz Naufal <azizeternaly@gmail.com>
2022-10-16 11:45:31 +0700

    Commit take-out bgm from startup jarvis

--- Python/PercobaanVoiceRecognition/StartupJarvis/main.py
+++ Python/PercobaanVoiceRecognition/StartupJarvis/main.py
@@ -4,17 +4,17 @@ from Python.PercobaanVoiceRecognition.PersonalAssistant.head import Head
 import time
 from jazzit import waiting_track
 
-@waiting_track("D:\MINE\Project\Personal\Mainan\Python\PercobaanVoiceRecognition\\asset\sound\mixkit-sci-fi-drone-hover-915.wav")
+# @waiting_track("D:\MINE\Project\Personal\Mainan\Python\PercobaanVoiceRecognition\\asset\sound\mixkit-dark-synth-oscillator-646.wav")
 
 def run():
     jarvisHead = Head("en", 180)
-    time.sleep(5)
+    time.sleep(2)
     text = "Wellcome Sir!"
     jarvisHead.speak(text)
-    time.sleep(0.5)
+    time.sleep(0.25)
     text = " All System are ready to lunch!"
     jarvisHead.speak(text)
-    time.sleep(2)
+    time.sleep(1.25)
 
 
 if __name__ == "__main__":
